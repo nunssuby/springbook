03ee798c06e93e787606eec9c3aa353fea7abfc6
nunssuby <nunssuby@gmail.com>
2015-03-03 15:55:03 +0900

    1.1.3까지 DB는 회사DB 사용함

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -11,8 +11,8 @@ import springbook.user.domain.User;
 public class UserDao {
 	public void add(User user) throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?characterEncoding=UTF-8", "spring",
-				"book");
+		Connection c = DriverManager.getConnection("jdbc:mysql://10.10.2.222/springbook?characterEncoding=UTF-8", "nunssuby",
+				"sindo2007");
 
 		PreparedStatement ps = c.prepareStatement(
 			"insert into users(id, name, password) values(?,?,?)");
@@ -29,8 +29,8 @@ public class UserDao {
 
 	public User get(String id) throws ClassNotFoundException, SQLException {
 		Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook?characterEncoding=UTF-8", "spring",
-				"book");
+		Connection c = DriverManager.getConnection("jdbc:mysql://10.10.2.222/springbook?characterEncoding=UTF-8", "nunssuby",
+				"sindo2007");
 		PreparedStatement ps = c
 				.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
